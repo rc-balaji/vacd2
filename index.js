952b498fca32aa5f6c8bf95efe691ffafd87fcfe
rc-balaji <balajic@drngpit.ac.in>
2023-07-25 15:28:19 +0530

    new

--- index.js
+++ index.js
@@ -2,4 +2,52 @@ console.log("Hello")
 
 var a=10;
 
-console.log(a)
+// console.log(a)
+
+
+// let name="Balaji"
+// let age =12
+// let clg=false
+// let tech=["app","web","web"]
+// let app={
+//     name:'john',
+//     age:23,
+//     develop:"Full Stack Developer"
+// }
+
+// let sal=null
+
+// console.log(`My name is ${name} and my age is ${age} ,i Studied ${clg} in ${tech} ,My intrest ${app.develop} ,My Salery ${sal}`)
+
+// alert("Helloo")
+
+// var a=prompt("Enter N:")
+// console.log(a)
+
+
+// var a=prompt("Enter A:")
+// var b=prompt("Enter B:")
+
+// alert(`${a} + ${b} = ${a+b}`)
+
+// console.log(`${a} + ${b} = ${a+b}`)
+
+
+// var name=prompt("What's your Name:")
+
+// alert("Welcome "+name+"!!!")
+
+
+const person={
+    id:7,
+    name:"Balaji",
+    age:21,
+    salery:5,
+
+}
+
+person.age=23
+document.write(person.id+"<br>")
+document.write(person.name+"<br>")
+document.write(person.age+"<br>")
+document.write(person.salery+"<br>")
